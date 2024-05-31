2581efbe7de08be91dc5cfce0f33d97bd9d60ee8
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-31 10:14:27 +0530

    Created a themeprovider using riverpod and connected to the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:dine_direct/pages/login_page.dart';
+import 'package:dine_direct/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Dine Direct',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ref.watch(themeProvider).themeData,
+      debugShowCheckedModeBanner: false,
       home: const LoginPage(),
     );
   }
---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,27 @@
+import 'package:dine_direct/themes/dark_mode.dart';
+import 'package:dine_direct/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final themeProvider = ChangeNotifierProvider((_) => ThemeProvider());
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
