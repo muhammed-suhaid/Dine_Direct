0eb98c5d080b1098c0b299c0cddbe684b0cfc32d
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-01 09:40:07 +0530

    Created a sliver appbar

---
+++ lib/components/my_sliver_appbar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MySliverAppBar extends StatelessWidget {
+  final Widget child;
+  final Widget title;
+  const MySliverAppBar({
+    super.key,
+    required this.child,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      expandedHeight: 340,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      actions: [
+        //cart button
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_cart),
+        ),
+      ],
+      title: const Text(
+        'Dine Direct',
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      ),
+      centerTitle: true,
+      flexibleSpace: FlexibleSpaceBar(
+        background: child,
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(
+          left: 0,
+          right: 0,
+          top: 0,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:dine_direct/components/my_drawer.dart';
+import 'package:dine_direct/components/my_sliver_appbar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,13 +13,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background, 
-      appBar: AppBar(
-        title: const Text('Home'),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.background,  
-      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      // appBar: AppBar(
+      //   title: const Text('Home'),
+      //   centerTitle: true,
+      //   backgroundColor: Theme.of(context).colorScheme.background,
+      // ),
       drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) =>[MySliverAppBar(
+          child: Text('Hello'),
+          title: Text('title'),
+        ),] ,
+        body: Container(
+          color: Colors.blue,
+        ),
+       
+      ),
     );
   }
 }
