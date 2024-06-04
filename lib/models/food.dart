efc6dcb45793c03caf0f16d9d85d61c6ad93b4d5
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-04 15:44:33 +0530

    Created a food model

---
+++ lib/models/food.dart
@@ -0,0 +1,38 @@
+//food item
+class Food {
+  final String name;
+  final String description;
+  final String imagePath;
+  final double price;
+  final FoodCategory category;
+  final List<Addon> availableAddons;
+
+  Food({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.price,
+    required this.category,
+    required this.availableAddons,
+  });
+}
+
+//food category
+enum FoodCategory {
+  burgeres,
+  salads,
+  sides,
+  desserts,
+  drinks,
+}
+
+//food addons
+class Addon {
+  final String name;
+  final double price;
+
+  Addon({
+    required this.name,
+    required this.price,
+  });
+}
