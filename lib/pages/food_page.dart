99eaca50848a4d13b0724cfdf0b3425493fe0fc6
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-05 15:38:55 +0530

    Added a back button and Addon checkbox completed

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -4,11 +4,17 @@ import 'package:flutter/material.dart';
 
 class FoodPage extends StatefulWidget {
   final Food food;
+  final Map<Addon, bool> selectedAddons = {};
 
-  const FoodPage({
+  FoodPage({
     super.key,
     required this.food,
-  });
+  }) {
+    //initilize selected addons to be false
+    for (Addon addon in food.availableAddons) {
+      selectedAddons[addon] = false;
+    }
+  }
 
   @override
   State<FoodPage> createState() => _FoodPageState();
@@ -17,104 +23,130 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            //food image
-            Image.asset(widget.food.imagePath),
+    return Stack(
+      children: [
+        //Scaffold UI
+        Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                //food image
+                Image.asset(widget.food.imagePath),
 
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //food name
-                  Text(
-                    widget.food.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-
-                  //food Price
-                  Text(
-                    '\$ ${widget.food.price.toString()}',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: 16,
-                    ),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  //food description
-                  Text(widget.food.description),
-
-                  const SizedBox(height: 10),
-                  Divider(color: Theme.of(context).colorScheme.tertiary),
-                  const SizedBox(height: 10),
-
-                  //addons
-                  Text(
-                    'Add-ons',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.tertiary,
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //food name
+                      Text(
+                        widget.food.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      padding: EdgeInsets.zero,
-                      itemCount: widget.food.availableAddons.length,
-                      itemBuilder: (context, index) {
-                        //get individual addon
-                        Addon addon = widget.food.availableAddons[index];
 
-                        //check box
-                        return CheckboxListTile(
-                          title: Text(addon.name),
-                          subtitle: Text(
-                            '\$ ${addon.price}',
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
+                      //food Price
+                      Text(
+                        '\$ ${widget.food.price.toString()}',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 16,
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      //food description
+                      Text(widget.food.description),
+
+                      const SizedBox(height: 10),
+                      Divider(color: Theme.of(context).colorScheme.tertiary),
+                      const SizedBox(height: 10),
+
+                      //addons
+                      Text(
+                        'Add-ons',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).colorScheme.tertiary,
                           ),
-                          value: false,
-                          onChanged: (value) {},
-                        );
-                      },
-                    ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          padding: EdgeInsets.zero,
+                          itemCount: widget.food.availableAddons.length,
+                          itemBuilder: (context, index) {
+                            //get individual addon
+                            Addon addon = widget.food.availableAddons[index];
+
+                            //check box
+                            return CheckboxListTile(
+                              title: Text(addon.name),
+                              subtitle: Text(
+                                '\$ ${addon.price}',
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                              value: widget.selectedAddons[addon],
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  widget.selectedAddons[addon] = value!;
+                                });
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+
+                //btton -> add to cart
+                MyButton(
+                  onTap: () {},
+                  text: 'Add to cart',
+                ),
+
+                const SizedBox(height: 25),
+              ],
+            ),
+          ),
+        ),
+
+        //back button
+        SafeArea(
+          child: Opacity(
+            opacity: 0.6,
+            child: Container(
+              margin: const EdgeInsets.only(left: 25),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  shape: BoxShape.circle),
+              child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(Icons.arrow_back_ios_new_rounded),
               ),
             ),
-
-            //btton -> add to cart
-            MyButton(
-              onTap: () {},
-              text: 'Add to cart',
-            ),
-
-            const SizedBox(height: 25),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
